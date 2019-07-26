afb25ca612f806334e4bd754611a35ba8ba9edd9
yannik <yannik>
2019-07-26 14:39:37 +0000

    cosmetic changes

--- sensor.py
+++ sensor.py
@@ -7,7 +7,7 @@ import utils
 import Log
 
 
-import time
+import time 
 import errno
 import socket
 import threading
@@ -384,27 +384,41 @@ class Camera(Sensor):
 
         self.captureEvent = threading.Event()
 
-        Log.Write('resolution %s - framerate %u' % (str(camera.resolution), camera.framerate))
+        Log.Write('camera.revision = %s' % str(camera.revision))
+        Log.Write('camera resolution %s - framerate %u' % (str(camera.resolution), camera.framerate))
 
         self.rawFps = FPS()
 
         self.frame = None
 
-        def cameraSettings():
+        def settings():
             time.sleep(5)
-            camera.exposure_mode = config.exposure_mode
-            camera.awb_mode = config.awb_mode
+
             camera.iso = config.iso
             camera.shutter_speed = config.shutter_speed
             camera.brightness = config.brightness
             camera.contrast = config.contrast
+
+            time.sleep(5)
+
+            Log.Write('camera.iso = %s' % str(camera.iso))
+            Log.Write('camera.exposure_speed = %s' % str(camera.exposure_speed))
+            Log.Write('camera.brightness = %s' % str(camera.brightness))
+            Log.Write('camera.contrast = %s' % str(camera.contrast))
+            Log.Write('camera.awb_gains = %s' % str(camera.awb_gains))
+            Log.Write('camera.digital_gain = %s' % str(camera.digital_gain))
+            Log.Write('camera.analog_gain = %s' % str(camera.analog_gain))
+
+            camera.exposure_mode = config.exposure_mode
+            camera.awb_mode = config.awb_mode
             camera.awb_gains = config.awb_gains
 
             time.sleep(5)
-            Log.Write('camera.revision = %s' % str(camera.revision))
+
             Log.Write('camera.exposure_mode = %s' % str(camera.exposure_mode))
             Log.Write('camera.awb_mode = %s' % str(camera.awb_mode))
             Log.Write('camera.iso = %s' % str(camera.iso))
+            Log.Write('camera.exposure_speed = %s' % str(camera.exposure_speed))
             Log.Write('camera.shutter_speed = %s' % str(camera.shutter_speed))
             Log.Write('camera.brightness = %s' % str(camera.brightness))
             Log.Write('camera.contrast = %s' % str(camera.contrast))
@@ -412,7 +426,7 @@ class Camera(Sensor):
             Log.Write('camera.digital_gain = %s' % str(camera.digital_gain))
             Log.Write('camera.analog_gain = %s' % str(camera.analog_gain))
 
-        utils.StartThread(cameraSettings, restart=False)
+        utils.StartThread(settings, restart=False)
 
 
 
